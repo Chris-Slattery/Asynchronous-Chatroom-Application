5285c2ced633185806cb16a3a94ada823bdad60b
Chris Slattery <chrisslattery08@gmail.com>
2023-12-01 16:35:46 +0000

    Initial commit, starting project with basic functionality

---
+++ core/routing.py
@@ -0,0 +1,16 @@
+#Class for routing data and using channels
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import chat.routing
+
+application = ProtocolTypeRouter({
+    'websocket': AuthMiddlewareStack(
+        URLRouter(
+            chat.routing.websocket_urlpatterns
+
+        )
+
+    ),
+
+})
